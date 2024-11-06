244ca1a44a9db301943292aeffb60de658cd7b20
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-11-06 08:53:59 +0000

    add completion support for all lsps, add html and gopls lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,6 +13,8 @@ return {
 					"lua_ls",
 					"ts_ls",
 					"phpactor",
+          "html",
+          "gopls",
 				},
 			})
 		end,
@@ -20,10 +22,23 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.phpactor.setup({})
+			lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+			lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+			lspconfig.phpactor.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
